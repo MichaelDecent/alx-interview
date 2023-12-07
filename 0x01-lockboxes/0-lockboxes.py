91a7180edb0f33895f33c6ba1fdde4fde08bfc11
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-07 14:56:33 +0100

    added solution to task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -28,21 +28,36 @@ def canUnlockAll(boxes):
     if not boxes or len(boxes) == 0:
         return False
 
-    box_status = {}
-
-    for index, value in enumerate(boxes):
-        box_status[index] = False
-
+    box_status = {index: False for index in range(len(boxes))}
     box_status[0] = True
-
+    
     for index, value in enumerate(boxes):
         for val in value:
-            if box_status[index] == True:
-                box_status[val] = True
+            if box_status[index] == True and 0 <= val < len(boxes):
+                box_status[val] = True         
             if val < index:
                 for val in boxes[index]:
                     box_status[val] = True
 
-    final_status = all(value for value in box_status.values())
+    return all(box_status.values())
 
-    return final_status          
+from collections import deque
+
+def canUnlockAll(boxes):
+    if not boxes or len(boxes) == 0:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True  # Starting from the first box which is unlocked
+    queue = deque([0])  # Start BFS with the first box
+
+    while queue:
+        current_box = queue.popleft()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -11,6 +11,6 @@ print(canUnlockAll(boxes))
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
 
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+boxes2 = [[1, 3], [3, 0, 1], [2], [0]]
+print(canUnlockAll(boxes2))
 
