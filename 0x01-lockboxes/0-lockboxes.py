912814cd3e7548c2ee2833fa3c276cf26477f414
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-07 13:12:52 +0100

    added solution to task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,22 +3,46 @@
 This program determines if all the boxes (list of lists) can be opened.
 """
 
-def canUnlockAll(boxes):
-    """
-    Return True if all boxes can be opened, else return False
-    """
-    keys = boxes[0]
-    box_checked = False
-    position = 0
-    index = 0
-    while index < len(boxes) - 1:
-        if index+1 in keys:
-            keys.extend(boxes[index+1])
-            position += 1
-            index = position
-            box_checked = True
-        else:
-            index += 1
-            box_checked = False
-    return box_checked        
+# def canUnlockAll(boxes):
+#     """
+#     Return True if all boxes can be opened, else return False
+#     """
+#     keys = boxes[0]
+#     box_checked = False
+#     position = 0
+#     index = 0
+#     while index < len(boxes) - 1:
+#         if index+1 in keys:
+#             keys.extend(boxes[index+1])
+#             position += 1
+#             index = position
+#             box_checked = True
+#         else:
+#             index += 1
+#             box_checked = False
+#     return box_checked        
             
+
+
+def canUnlockAll(boxes):
+    box_status = {}
+
+    for index, value in enumerate(boxes):
+        box_status[index] = False
+
+    box_status[0] = True
+
+    for index, value in enumerate(boxes):
+        for val in value:
+            if box_status[index] == True:
+                box_status[val] = True
+            if val < index:
+                for val in boxes[index]:
+                    box_status[val] = True
+
+    all_true = all(value in {True} for value in box_status.values())
+
+    return all_true
+
+    
+                
