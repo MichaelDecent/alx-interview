5a33d88dc07c26f7b50508daeac899d4f8fea111
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-22 15:34:50 +0100

    added solution to task 0

--- test.py
+++
@@ -1,6 +0,0 @@
-text = "This is a, sample text-with various separators!"
-separators = [" ", ",", "-"]  # List of separators
-
-# Split the string using multiple separators
-words = text.split(separators)
-print(words)
