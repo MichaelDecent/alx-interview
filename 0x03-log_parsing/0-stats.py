69ff6e3fb2b754eea48cecdda51a25bdc98da671
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-22 16:03:09 +0100

    added solution to task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,14 +8,12 @@ Total file size: File size: <total size>
 where <total size> is the sum of all previous <file size> (see input format above)
 Number of lines by status code:
 possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+if a status code doesnt appear or is not an integer, dont print anything for this status code
 format: <status code>: <number>
 status codes should be printed in ascending order
 
 """
 import sys
-from ipaddress import ip_address
-from datetime import datetime
 from typing import Dict
 
 
