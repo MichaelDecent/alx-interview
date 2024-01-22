ad43aba65a4371554c99d7181abc2ab1d832496a
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-22 11:53:50 +0100

    added a solution to task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,32 +2,40 @@
 
 const request = require('request');
 
-const baseUrl = 'https://swapi-api.alx-tools.com/api/';
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(`${baseUrl}films/${process.argv[2]}/`, (error, response, body) => {
-  if (error) {
+async function getRequest () {
+  try {
+    const result = await makeRequest(url);
+    for (const element of result.characters) {
+      try {
+        const data = await makeRequest(element);
+        console.log(data.name);
+      } catch (error) {
+        console.error('Request error:', error);
+      }
+    }
+  } catch (error) {
     console.error('Request error:', error);
-    return;
   }
+}
 
-  if (response.statusCode >= 200 && response.statusCode < 300) {
-    const data = JSON.parse(body);
+function makeRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(new Error(`Request error: ${error}`));
+        return;
+      }
 
-    data.characters.forEach((element) => {
-      request(element, (error, response, body) => {
-        if (error) {
-          console.error('Request error:', error);
-          return;
-        }
-        if (response.statusCode >= 200 && response.statusCode < 300) {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        } else {
-          console.error(`HTTP error! Status: ${response.statusCode}`);
-        }
-      });
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        const data = JSON.parse(body);
+        resolve(data);
+      } else {
+        reject(new Error(`HTTP error! Status: ${response.statusCode}`));
+      }
     });
-  } else {
-    console.error(`HTTP error! Status: ${response.statusCode}`);
-  }
-});
+  });
+}
+
+getRequest();
