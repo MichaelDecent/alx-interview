fd5d17b4db865534d3a763489c983900f0c525a2
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-25 16:06:30 +0100

    added solution to task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -13,6 +13,6 @@ def rotate_2d_matrix(matrix) -> None:
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    #reverse each row in the matrix
+    # reverse each row in the matrix
     for i in range(n):
         matrix[i].reverse()
