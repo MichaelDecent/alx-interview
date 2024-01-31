16c9059a7e705897f4f89d9c98b61aac2675cdbd
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-31 14:25:24 +0100

    added solution to task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This Module contains a function that determines the
+fewest number of coins needed to meet a given amount 
+"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of
+    coins needed to meet a given amount
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+
+    count_list = [coins[0]]
+
+    for value in coins:
+        current_total = sum(count_list)
+        remainder = total % sum(count_list)
+        while current_total < total and remainder >= value:
+            count_list.append(value)
+            current_total = sum(count_list)
+            if current_total == total:
+                return len(count_list)
+            elif current_total > total:
+                return -1
